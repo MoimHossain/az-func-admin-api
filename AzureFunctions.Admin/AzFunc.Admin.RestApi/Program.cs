c3b2c18f6fba20f7b07e31afde4afcd62c6c18cc
Moim HA <moimhossain@gmail.com>
2020-01-24 12:59:58 +0100

    Added sample API invocation
    Added sample API invocation

---
+++ AzureFunctions.Admin/AzFunc.Admin.RestApi/Program.cs
@@ -0,0 +1,63 @@
+﻿
+
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AzFunc.Admin.RestApi
+{
+    class Program
+    {
+        static HttpClient httpClient = new HttpClient();
+        static void Main(string[] args)
+        {
+            var funcAppName = "azfunchostingdd";
+            var kudoUserName = Environment.GetEnvironmentVariable("kudoUserName");
+            var kudoPassword = Environment.GetEnvironmentVariable("kudoPassword");
+            var kudoBaseUrl = $"https://{funcAppName}.scm.azurewebsites.net/api";
+            var siteBaseUrl = $"https://{funcAppName}.azurewebsites.net";
+
+            var base64AuthInfo = Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes($"{kudoUserName}:{kudoPassword}"));
+            var token = GetAdminTokenAsync(kudoBaseUrl, base64AuthInfo).Result;
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            
+            var status = GetHostStatusAsync(siteBaseUrl).Result;
+
+            Console.WriteLine($"Token: {token}");
+        }
+
+        static async Task<HostStatus> GetHostStatusAsync(string siteBaseUrl)
+        {
+            var response = await httpClient.GetAsync($"{siteBaseUrl}/admin/host/status");  
+
+            return await GetObjectAsync<HostStatus>(response);
+        }
+
+        static async Task<string> GetAdminTokenAsync(string kudoBaseUrl, string base64AuthInfo)
+        {
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", base64AuthInfo);
+            var response = await httpClient.GetAsync($"{kudoBaseUrl}/functions/admin/token");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var body = await response.Content.ReadAsStringAsync();
+                return body.Replace("\"", string.Empty);
+            }
+            return string.Empty;
+        }
+
+        static async Task<T> GetObjectAsync<T>(HttpResponseMessage response) where T : new()
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                var body = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<T>(body);
+            }
+            return default(T);
+        }
+    }
+}
---
+++ AzureFunctions.Admin/AzFunc.Admin.RestApi/Schema.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzFunc.Admin.RestApi
+{
+    public class HostStatus
+    {
+        public string id { get; set; }
+        public string state { get; set; }
+        public string version { get; set; }
+        public string versionDetails { get; set; }
+        public int processUptime { get; set; }
+    }
+}
